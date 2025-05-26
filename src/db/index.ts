9f61383357250b46efc83257d5133601f0e928f2
tabetant <antoine.tabet@mail.utoronto.ca>
2025-05-26 15:56:21 +0300

    replaced template 'users' table with 'tasks'

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,9 @@
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
-import { users } from './drizzle/schema'
+import { tasks } from './drizzle/schema'
 
 // Disable prefetch as it is not supported for "Transaction" pool mode
 const client = postgres(process.env.DATABASE_URL!, { prepare: false })
 const db = drizzle(client);
 
-const allUsers = await db.select().from(users);
+const allUsers = await db.select().from(tasks);
